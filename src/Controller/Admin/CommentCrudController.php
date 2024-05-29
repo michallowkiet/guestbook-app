0e8e9555cd99034d6528016118098f245ff4ee67
Michał Łowkiet <michallowkiet@gmail.com>
2024-05-29 14:07:57 +0200

    Added: pagination

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -3,10 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class CommentCrudController extends AbstractCrudController
 {
@@ -15,14 +20,37 @@ class CommentCrudController extends AbstractCrudController
         return Comment::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->setEntityLabelInSingular('Conference comment')
+            ->setEntityLabelInPlural('Conference comments')
+            ->setSearchFields(['author', 'text', 'email'])
+            ->setDefaultSort(['createdAt' => 'DESC']);
+    }
+
+    public function configureFilters(Filters $filters):Filters
+    {
+        return $filters->add(EntityFilter::new('conference'));
+    }
+
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield AssociationField::new('conference');
+        yield TextField::new('author');
+        yield EmailField::new('email');
+        yield TextAreaField::new('text')->hideOnIndex();
+        yield TextField::new('photoFilename')->hideOnIndex();
+        $createAt = DateTimeField::new('createdAt')->setFormTypeOptions([
+            'years' => range(date('Y'), date('Y', strtotime('+5 year'))),
+            'widget' => 'single_text',
+        ]);
+
+        if (Crud::PAGE_EDIT === $pageName) {
+            yield $createAt->setFormTypeOption('disabled', true);
+        } else {
+            yield $createAt;
+        }
     }
-    */
+
 }
